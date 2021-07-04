da6b3bf8871678221ecda0498999be08e1396e1d
Chantelsky <laver.chantelsky@gmail.com>
2021-07-04 21:49:30 +1000

    added resolver

---
+++ start/server/src/resolvers.js
@@ -0,0 +1,34 @@
+module.exports = {
+  Query: {
+    launches: (_, __, { dataSources }) =>
+      dataSources.launchAPI.getAllLaunches(),
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
+  },
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
+    },
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      /**
+       * gets ids of launches by the user
+       */
+      const launchIds = await dataSources.userAPI.getLuanchIdsByUser();
+      if (!launchIds.length) return [];
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
+};
